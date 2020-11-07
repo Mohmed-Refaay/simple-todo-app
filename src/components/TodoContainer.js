b63bc32bedeca2bb43823603ac51ee0a69795d09
unknown <mohamedamrrefaay@gmail.com>
2020-11-07 17:25:21 +0200

    testing is completed

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -12,7 +12,7 @@ class TodoContainer extends React.Component {
         // id: uuid.v4(),
         id: uuidv4(),
         title: "Setup development environment",
-        completed: true,
+        completed: false,
       },
       {
         // id: uuid.v4(),
@@ -27,17 +27,43 @@ class TodoContainer extends React.Component {
         completed: false,
       },
     ],
+    completedTodos: [],
   };
 
   handleChange = (id) => {
-    this.setState({
-      todos: this.state.todos.map((todo) => {
-        if (todo.id === id) {
-          todo.completed = !todo.completed;
-        }
-        return todo;
-      }),
+    const clickedTodoItemIdex = this.state.todos.findIndex((todo) => {
+      return todo.id == id;
     });
+    const clickedCompletedTodoItemIdex = this.state.completedTodos.findIndex((todo) => {
+      return todo.id == id;
+    });
+    if (clickedTodoItemIdex >= 0) {
+      const completedTodoItem = this.state.todos[clickedTodoItemIdex];
+      completedTodoItem.completed = true;
+      this.setState({
+        todos: [
+          ...this.state.todos.filter((todo) => {
+            return todo.id !== id;
+          }),
+        ],
+        completedTodos: [...this.state.completedTodos, completedTodoItem],
+      });
+      console.log(completedTodoItem);
+    }
+    if (clickedCompletedTodoItemIdex >= 0) {
+      const unCompletedTodoItem = this.state.todos[clickedCompletedTodoItemIdex];
+      unCompletedTodoItem.completed = false;
+      this.setState({
+        todos: [
+          ...this.state.todos, unCompletedTodoItem
+        ],
+        completedTodos: [
+          ...this.state.completedTodos.filter((completedTodo) => {
+            return completedTodo.id !== id;
+          })
+        ]
+      });
+    }
   };
 
   delTodo = (id) => {
@@ -69,6 +95,12 @@ class TodoContainer extends React.Component {
         <InputTodo addTodoProps={this.addTodoItem} />
         <TodosList
           todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
+        />
+        <h1>Completed Tasks</h1>
+        <TodosList
+          todos={this.state.completedTodos}
           handleChangeProps={this.handleChange}
           deleteTodoProps={this.delTodo}
         />
